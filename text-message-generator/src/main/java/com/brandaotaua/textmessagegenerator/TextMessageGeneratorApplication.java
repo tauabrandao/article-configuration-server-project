8c56f5c6cef69297494320b67e7f69b3f525c1b6
tauabrandao <brandao.taua@gmail.com>
2021-06-01 17:29:34 -0300

    [text-message-generator] project started

---
+++ text-message-generator/src/main/java/com/brandaotaua/textmessagegenerator/TextMessageGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.brandaotaua.textmessagegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextMessageGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TextMessageGeneratorApplication.class, args);
+    }
+
+}
---
+++ text-message-generator/src/test/java/com/brandaotaua/textmessagegenerator/TextMessageGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brandaotaua.textmessagegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextMessageGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
